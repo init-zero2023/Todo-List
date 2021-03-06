baefa9f2f24713c0cfcd6aac0fd5f3bb9b73f927
init-zero2023 <santosh.sk11112222@gmail.com>
2021-03-06 17:38:06 +0530

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const _ = require('lodash');
-const port = 3000;
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://admin-santosh:2019UCO1523@mycluster.1xj5o.mongodb.net/todolistDB?retryWrites=true&w=majority", { useNewUrlParser: true });
 
 const itemSchema = {
     task: String,
@@ -127,6 +126,10 @@ app.post('/delete', (req, res) => {
     // console.log(req.body.checkbox);
 });
 
-app.listen(3000 || process.env.PORT, () => {
-    console.log(`Server is listening to port ${3000}`);
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+app.listen(port, () => {
+    console.log(`Server is listening to port ${port}`);
 });
